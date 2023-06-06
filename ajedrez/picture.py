ae70f1fbe88ae22927c53a41dcf693ffe06dcbb9
Gonzalo01703 <glaymem@unsa.edu.pe>
2023-06-06 11:10:37 -0500

    Ejercicios 2f,2g

--- ajedrez/picture.py
+++ ajedrez/picture.py
@@ -1,7 +1,9 @@
+from importlib.resources import contents
+from shelve import Shelf
 from colors import *
 class Picture:
   def __init__(self, img):
-    self.img = img
+    self.img = img;
 
   def __eq__(self, other):
     return self.img == other.img
@@ -11,42 +13,83 @@ class Picture:
       return color
     return inverter[color]
 
-  def verticalMirror(self):
+  def verticalMirror(self, ):
     """ Devuelve el espejo vertical de la imagen """
     vertical = []
     for value in self.img:
-    	vertical.append(value[::-1])
-    return vertical
+      vertical.append(value[::-1])
+    return Picture(vertical)
 
   def horizontalMirror(self):
     """ Devuelve el espejo horizontal de la imagen """
-    return Picture(None)
+    horizontal = []
+    length = len(self.img)
+    for x in range(length):
+      horizontal.append(self.img[length-x-1])
+    return Picture(horizontal)
 
   def negative(self):
     """ Devuelve un negativo de la imagen """
-    return Picture(None)
+    content = []
+    x = 0
+    while x < len(self.img):
+      content.append("")
+      y = 0
+      while y < len(self.img[x]):
+        content[x] += self._invColor(self.img[x][y])
+        y += 1
+      x += 1
+    return Picture(content)
 
   def join(self, p):
     """ Devuelve una nueva figura poniendo la figura del argumento 
         al lado derecho de la figura actual """
-    return Picture(None)
-
+    count = 0
+    content = []
+    for x in self.img:
+      content.append(x)
+    for y in p.img:
+      content[count] += y
+      count = count+1
+    return Picture(content)
   def up(self, p):
-    return Picture(None)
+    content = p.img + self.img
+    return Picture(content)
 
   def under(self, p):
     """ Devuelve una nueva figura poniendo la figura p sobre la
         figura actual """
-    return Picture(None)
+    Content = []
+    for x in p.img:
+      Content.append(x)
+    for x in range(len(p.img)):
+      for y in range(len(p.img[x])):
+        Content[x] = p.img[x].replace(" ",self.img[x][y])
+    return Picture(Content)
   
   def horizontalRepeat(self, n):
     """ Devuelve una nueva figura repitiendo la figura actual al costado
         la cantidad de veces que indique el valor de n """
-    return Picture(None)
+    content = []
+    for x in self.img:
+      content.append(x)
+    while(n > 0):
+      count = 0
+      for y in self.img:
+        content[count] += y
+        count += 1
+      n -= 1
+    return Picture(content)    
 
   def verticalRepeat(self, n):
-    return Picture(None)
-
+    content = []
+    for x in self.img:
+      content.append(x)
+    while(n > 0):
+      content += self.img
+      n -= 1
+    return Picture(content)
+    
   #Extra: Sólo para realmente viciosos 
   def rotate(self):
     """Devuelve una figura rotada en 90 grados, puede ser en sentido horario
